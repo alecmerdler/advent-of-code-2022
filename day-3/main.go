1676f7d268c575ffa7938f095eaa5f6e26e72fd9
Alec Merdler <alecmerdler@gmail.com>
2023-11-05 15:01:33 -0500

    day-3 attempt 1

--- day-3/main.go
+++ day-3/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
+	"strings"
 )
 
 func main() {
-	defer duration(track("foo"))
-
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		panic(err)
@@ -23,12 +21,40 @@ func main() {
 	if _, err := file.Read(raw); err != nil {
 		panic(err)
 	}
+
+	total := 0
+	lines := strings.Split(string(raw), "\n")
+	for _, line := range lines {
+		foundAt := map[rune]int{}
+		for index, item := range line {
+			if pos, ok := foundAt[item]; ok && sameCompartment(index, pos, len(line)) {
+				// Flip the character from upper->lower or vice-versa so we can use the ASCII value
+				var flipped string
+				if lowered := strings.ToLower(string(item)); lowered != string(item) {
+					fmt.Printf("flipping %s -> %s\n", string(item), lowered)
+					flipped = lowered
+				} else {
+					fmt.Printf("flipping %s -> %s\n", string(item), strings.ToUpper(string(item)))
+					flipped = strings.ToUpper(string(item))
+				}
+
+				asciiCode := int(flipped[0])
+				priority := int(asciiCode) - 64
+				total += priority
+
+				// FIXME(alecmerdler): Seeing what the int value of each rune is...
+				fmt.Printf("%s - %s, %d, %d\n", line, string(item), priority, total)
+
+				break
+			}
+
+			foundAt[item] = index
+		}
+	}
+
+	fmt.Println(total)
 }
 
-func track(msg string) (string, time.Time) {
-	return msg, time.Now()
-}
-
-func duration(msg string, start time.Time) {
-	fmt.Printf("%v: %v\n", msg, time.Since(start))
+func sameCompartment(posA, posB, totalSize int) bool {
+	return (posA < totalSize/2 && posB < totalSize/2) || (posA >= totalSize/2 && posB >= totalSize/2)
 }
